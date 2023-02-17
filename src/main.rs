0e17769e59985f5fb45495b6f8f6c410a2965312
flooijdt <flooijdt@posteo.de>
2023-02-17 09:53:44 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,21 @@ fn main() {
 }
 
 fn solution(num: i32) -> i32 {
-    for i in 1..num {
-        println!("{:?}", i);
+    let mut sum: i32 = 0;
+    if num < 0 {
+        return 0;
+    } else {
+        for i in 1..num {
+            if i % 3 == 0 {
+                sum += i;
+            } else {
+                if i % 5 == 0 {
+                    sum += i;
+                } else {
+                    continue;
+                }
+            }
+        }
     }
-    0
+    sum
 }
