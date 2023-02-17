b412b4431819fc0e83bfadb5caf61f9870220a00
flooijdt <flooijdt@posteo.de>
2023-02-17 09:38:45 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    println!("{:?}", solution(10));
+}
+
+fn solution(num: i32) -> i32 {
+    for i in 1..num {
+        println!("{:?}", i);
+    }
+    0
 }
